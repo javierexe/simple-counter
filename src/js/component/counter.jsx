389cea7c2da3d8654f61a72acbb370f522659f55
Javier Valenzuela <137245545+javierexe@users.noreply.github.com>
2025-03-18 18:08:05 +0000

    proyecto listo para entrega con requeirmientos básicos

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from "react";
 
 
+
 const SecondsCounter = () => {
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
+
     const interval = setInterval(() => {
       setSeconds((valorPrevio) => valorPrevio + 1);
     }, 1000);
 
-    return () => clearInterval(interval); // Limpia el intervalo al desmontar el componente
-  }, []); // Solo se ejecuta al montar el componente
+    return () => clearInterval(interval); 
+  }, []);
 
   const cadenaDigitos = seconds.toString().padStart(6, "0");
 
+
+
   return (
       <div id="counter">
-        {/* Ícono FontAwesome */}
-        <i className="fa-regular fa-clock fa-lg" style={{ color: "#ffffff"}}></i>
+        {/* Ícono Reloj */}
+        <i className="fa-regular fa-clock fa-lg m-2" style={{ color: "#ffffff"}}></i>
     
-        {/* Dígitos */}
+        {/* Segundos */}
         {cadenaDigitos.split("").map((digit, index) => (
           <div key={index} className="digit">
             {digit}
